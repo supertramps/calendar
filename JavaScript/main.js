089d23ef48730a4ff48008293a46eed47982316c
Alexander Liljedahl <alexander.liljedahl@live.se>
2020-11-20 15:57:32 +0100

    Javascript now renders days in the calendar

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,6 +4,12 @@ function main() {}
 
 const date = new Date();
 
+const monthDays = document.querySelector(".days");
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+console.log(lastDay);
+
 const months = [
   "January",
   "February",
@@ -20,3 +26,10 @@ const months = [
 ];
 
 document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+let days = "";
+
+for (let i = 1; i <= lastDay; i++) {
+  days += `<div>${i}</div>`;
+  monthDays.innerHTML = days;
+}
