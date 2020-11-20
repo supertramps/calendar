4b9ddd433f97286c78084a7720ed4b6ab933ef70
Alexander Liljedahl <alexander.liljedahl@live.se>
2020-11-20 11:27:34 +0100

    Added js to show current month in calendar header

---
+++ JavaScript/main.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", main());
+
+function main() {}
+
+const date = new Date();
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+document.querySelector(".date h1").innerHTML = months[date.getMonth()];
