205eaaa27ac5f51eb7feaab4beebaff14a10e18a
Alexander Liljedahl <alexander.liljedahl@live.se>
2020-11-23 10:50:54 +0100

    Code now runs variables before main()

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,35 +1,44 @@
+const date = new Date();
+date.setDate(1);
+const monthDays = document.querySelector(".days");
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+const firstDayIndex = date.getDay();
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
 window.addEventListener("load", main());
 
-function main() {}
+function main() {
+  getMonthsCurrent();
+  renderDays();
+}
 
-const date = new Date();
+function getMonthsCurrent() {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+}
 
-const monthDays = document.querySelector(".days");
+function renderDays() {
+  let days = "";
 
-const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
 
-console.log(lastDay);
-
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
-let days = "";
-
-for (let i = 1; i <= lastDay; i++) {
-  days += `<div>${i}</div>`;
-  monthDays.innerHTML = days;
+  for (let i = 1; i <= lastDay; i++) {
+    days += `<div>${i}</div>`;
+    monthDays.innerHTML = days;
+  }
 }
