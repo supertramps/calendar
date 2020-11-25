659abe857684b2637f70952cce0522a784d17bc9
Alexander Liljedahl <alexander.liljedahl@live.se>
2020-11-25 16:17:36 +0100

    Deleted file

--- JavaScript/secondApproach.js
+++
@@ -1,8 +0,0 @@
-let firstDay = (new Date(year, month)).getDay();
-
-// Calculate number of days within month
-
-daysInMonth(iMonth, iYear) {
-    return 32 - new Date(iYear, iMonth, 32).getDate();
-
-};
