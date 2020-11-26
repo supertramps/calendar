99215c945b8b704cc2509c97c6c1012b4d45a49d
Sebastian Zazzi <sebastian@zazzi.se>
2020-11-26 20:34:22 +0100

    added some styling plus some more work on the to do list

--- JavaScript/todos.js
+++ JavaScript/todos.js
@@ -1,4 +1,4 @@
-const toDosState = [];
+let toDosState = [];
 /**
  * function to add a To dos to the array of to dos
  */
@@ -9,59 +9,54 @@ function addToDos(title, date, time) {
     time,
   };
   toDosState.push(toDo);
-  
 }
 
-const form = document.getElementById('add-event-form');
-form.addEventListener('submit', event => {
+const form = document.getElementById("add-event-form");
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const whatInput = document.getElementById('what-event');
-  const whenInput = document.getElementById('what-event');
-  const wereInput = document.getElementById('what-event');
+  const whatInput = document.getElementById("what-event");
+  const whenInput = document.getElementById("when-event");
+  const whereInput = document.getElementById("where-event");
 
   const what = whatInput.value;
-  if (what !== '') {
-    addToDos(what);
-    whatInput.value = '';
-    whatInput.focus();
-    console.log(toDosState);
-  }
   const when = whenInput.value;
-  if (when !== '') {
-    addToDos(when);
-    whenInput.value = '';
-    whenInput.focus();
-  }
-  const where = whatInput.value;
-  if (what !== '') {
-    addToDos(what);
-    whatInput.value = '';
+  const where = whereInput.value;
+  if (what !== "" && when !== "" && where !== "") {
+    addToDos(what, where, when);
+    whatInput.value = "";
+    whenInput.value = "";
+    whereInput.value = "";
     whatInput.focus();
+    whenInput.focus();
+
     console.log(toDosState);
+    
   }
+
+  console.log(what);
+  console.log(where);
+  console.log(when);
   renderToDos();
-})
+});
 
 /**
  * function to open the Add Event meny
  */
 function openAddEventWindow() {
   const toDoButton = document.getElementById("new-event-button");
-  toDoButton.addEventListener('click', toggleWindow);
-  const window = document.querySelector('#toggleHidden')
-  const addEventButton = document.getElementById('close-event-window');
-  addEventButton.addEventListener('click', toggleWindow);
+  toDoButton.addEventListener("click", toggleWindow);
+  const window = document.querySelector("#toggleHidden");
+  const addEventButton = document.getElementById("close-event-window");
+  addEventButton.addEventListener("click", toggleWindow);
+  const submitBtn = document.getElementById("btn-for-submit");
+  submitBtn.addEventListener("click", toggleWindow);
   /**
    * function to open and close the window on button press
    */
   function toggleWindow() {
-    window.classList.toggle('hidden');
+    window.classList.toggle("hidden");
   }
 }
-
-
-
-
 
 /**
  * renders the array of objects containing the ToDos to the DOM
@@ -69,7 +64,7 @@ function openAddEventWindow() {
 function renderToDos() {
   const toDoContainer = document.querySelector(".todo-container");
 
-  for (let i = 0; i < toDosState.length; i++) {
+  for (let i = 1; i < toDosState.length; i++) {
     let appointmentTitle = document.createElement("h2");
     let appointmentTime = document.createElement("span");
 
