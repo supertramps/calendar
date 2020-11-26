b784c819fd1985463d662720e39edbf87e574345
Alexander Liljedahl <alexander.liljedahl@live.se>
2020-11-26 14:58:29 +0100

    Small fixes and styling

--- JavaScript/todos.js
+++ JavaScript/todos.js
@@ -17,9 +17,19 @@ function renderToDos() {
   for (const todo of todosState) {
     let appointment = "";
 
-    appointment += `<h2 id="task-1-container">${todo.title}</h2>`;
+    appointment += `<h2 class="task-title">${todo.title}</h2>`;
     //  appointment += '</br>';
-    appointment += `<span id="task-1-container">${todo.time}</span>`;
+    appointment += `<span class="task-time">${todo.time}</span>`;
     toDoContainer.innerHTML = appointment;
   }
 }
+
+//Shows current time in the ToDo-list
+const toDoTime = document.getElementById("time");
+setInterval(function updateTime() {
+  const date = new Date();
+  var string = date.toLocaleTimeString([], { timeStyle: "short" });
+  console.log(string);
+
+  toDoTime.innerHTML = string;
+}, 1000);
