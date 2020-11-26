407a886acb009d915aa600471eaa4425bb376642
Sebastian Zazzi <sebastian@zazzi.se>
2020-11-26 14:01:35 +0100

    some formating

--- JavaScript/todos.js
+++ JavaScript/todos.js
@@ -1,33 +1,25 @@
-const todosState = [{
-    title: 'Middag med katten',
+const todosState = [
+  {
+    title: "Middag med katten",
     date: new Date(),
-    time: '19:00'
-  }]
-
+    time: "19:00",
+  },
+];
 
 function addToDos() {
-
-    renderToDos();
+  renderToDos();
 }
 
 function renderToDos() {
+  const toDoContainer = document.querySelector(".todo-container");
+  toDoContainer.innerHTML = "";
 
-    
+  for (const todo of todosState) {
+    let appointment = "";
 
-    const toDoContainer = document.querySelector('.todo-container');
-    toDoContainer.innerHTML = "";
-
-    for (const todo of todosState) {
-        let appointment = '';
-
-       appointment += `<h2 id="task-1-container">${todo.title}</h2>`;
-     //  appointment += '</br>';
-       appointment += `<span id="task-1-container">${todo.time}</span>`;
-    
-    
-        toDoContainer.innerHTML = appointment;
-
-
-    }
-   
+    appointment += `<h2 id="task-1-container">${todo.title}</h2>`;
+    //  appointment += '</br>';
+    appointment += `<span id="task-1-container">${todo.time}</span>`;
+    toDoContainer.innerHTML = appointment;
+  }
 }
