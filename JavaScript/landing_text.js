1a9fb84b517d67ea1f5045dc52e72ad791660842
Sebastian Zazzi <Sebbelsinen@Zazzi-MPB.local>
2020-12-01 16:15:40 +0100

    minor fixes adn added day/night cycle

--- JavaScript/landing_text.js
+++ JavaScript/landing_text.js
@@ -1,6 +1,8 @@
 const textField = document.getElementById("random-landing-text");
 const timeField = document.getElementById("landing-time");
 
+
+// list of greetings
 const listOfGreetings = [
   "Hey User, another day another dollar am I right.",
   "Hey User, how's your day going?",
@@ -10,6 +12,10 @@ const listOfGreetings = [
 
 const greetingsPhrase = pickRandomPhrase();
 
+
+/**
+ * picks a random pharese from listOFGreetings
+ */
 function pickRandomPhrase() {
   const random = Math.floor(Math.random() * listOfGreetings.length);
   const randomGreetingPhrase = listOfGreetings[random];
@@ -17,13 +23,43 @@ function pickRandomPhrase() {
   return randomGreetingPhrase;
 }
 
+/**
+ * prints the time of the day and the greeting pharase to DOM
+ */
 function printPhraseAndTimeToDom() {
   pickRandomPhrase();
   textField.innerHTML = greetingsPhrase;
 
   setInterval(function updateTime() {
-    const date = new Date();
     
     timeField.innerHTML = date.toDateString() + " " + date.toLocaleTimeString('sv-SE');
   }, 100);
+
+/**
+ * Changes the background of sidebar depending on if its day or night
+ */
+setInterval
+(function changeDayNight() {
+  
+    const timeForCycle = date.getHours();
+  
+    console.log(timeForCycle);
+  
+    //night 
+    if (timeForCycle.to > 19 && timeForCycle < 5) {
+      document.getElementById('side').style.backgroundImage="url(./imgs/Task-bar.png)"
+      // morning 
+    } else if (timeForCycle > 5 && timeForCycle < 9) {
+      document.getElementById('side').style.background="url(./imgs/Task-bar.png)linear-gradient(180deg, #47CFFA 23.96%, rgba(95, 248, 238, 0.84) 47.49%, rgba(225, 255, 162, 0.66) 92.01%);"      
+      
+      //day
+    } else {
+      document.getElementById('side').style.background="url(./imgs/Task-bar.png)linear-gradient(180deg, #FED97C 23.96%, rgba(255, 188, 109, 0.84) 59.9%, rgba(255, 136, 109, 0.33) 100%);"      
+
+    }
+    
+  
+  
+  }, 1000 );
 }
+
